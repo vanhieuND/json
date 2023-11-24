531240680856cbe44e2d1843c519ec0904e65225
vanhieuND <88925703+vanhieuND@users.noreply.github.com>
2023-11-24 20:18:54 +0700

    Create load-thuoc.php

---
+++ load-thuoc.php
@@ -0,0 +1,107 @@
+<?php 
+/**
+ * Mẫu dùng để tra cứu thuốc 
+ * 
+ * POST qua CURL hoặc ajax: q=?&key=...
+ * key mặc định la dinhvanhieu
+ */
+echo json_encode(array(
+    array(
+        'maVatTu'=>'YHCT23001',
+        'nhomVatTu'=>'09. Thuốc vật tư y tế',
+        'tenThuongMai'=>'Đỗ trọng',
+        'hoatChat'=>'Đỗ trọng',
+        'donVi'=>'gram',
+        'duongDung'=>'',
+        'soLuong'=>12345,
+        'donGiaNhap'=>'',
+        'donGiaBan'=>220,
+        'ngaySanXuat'=>'2023-11-01 00:00:00',
+        'ngayHetHan'=>'2023-11-01 00:00:00',
+        'nhaSanXuat'=>'CTY dược ABC'
+    ),
+    array(
+        'maVatTu'=>'YHCT23002',
+        'nhomVatTu'=>'09. Thuốc vật tư y tế',
+        'tenThuongMai'=>'Vị thuốc y học cổ truyền Đỗ trọng',
+        'hoatChat'=>'Vị thuốc y học cổ truyền Đỗ trọng',
+        'donVi'=>'gram',
+        'duongDung'=>'',
+        'soLuong'=>12345,
+        'donGiaNhap'=>'',
+        'donGiaBan'=>220,
+        'ngaySanXuat'=>'2023-11-01 00:00:00',
+        'ngayHetHan'=>'2023-11-01 00:00:00',
+        'nhaSanXuat'=>'CTY dược ABC'
+    ),
+    array(
+        'maVatTu'=>'YHCT23003',
+        'nhomVatTu'=>'09. Thuốc vật tư y tế',
+        'tenThuongMai'=>'Hà thủ ô đỏ',
+        'hoatChat'=>'Hà thủ ô đỏ',
+        'donVi'=>'gram',
+        'duongDung'=>'',
+        'soLuong'=>12345,
+        'donGiaNhap'=>'',
+        'donGiaBan'=>220,
+        'ngaySanXuat'=>'2023-11-01 00:00:00',
+        'ngayHetHan'=>'2023-11-01 00:00:00',
+        'nhaSanXuat'=>'CTY dược ABC'
+    ),
+    array(
+        'maVatTu'=>'YHCT23004',
+        'nhomVatTu'=>'09. Thuốc vật tư y tế',
+        'tenThuongMai'=>'Cẩu tích',
+        'hoatChat'=>'Cẩu tích',
+        'donVi'=>'gram',
+        'duongDung'=>'',
+        'soLuong'=>12345,
+        'donGiaNhap'=>'',
+        'donGiaBan'=>220,
+        'ngaySanXuat'=>'2023-11-01 00:00:00',
+        'ngayHetHan'=>'2023-11-01 00:00:00',
+        'nhaSanXuat'=>'CTY dược ABC'
+    ),
+    array(
+        'maVatTu'=>'YHCT23005',
+        'nhomVatTu'=>'09. Thuốc vật tư y tế',
+        'tenThuongMai'=>'Đương quy di thực',
+        'hoatChat'=>'Đương quy di thực',
+        'donVi'=>'gram',
+        'duongDung'=>'',
+        'soLuong'=>12345,
+        'donGiaNhap'=>'',
+        'donGiaBan'=>220,
+        'ngaySanXuat'=>'2023-11-01 00:00:00',
+        'ngayHetHan'=>'2023-11-01 00:00:00',
+        'nhaSanXuat'=>'CTY dược ABC'
+    ),
+    array(
+        'maVatTu'=>'YHCT23006',
+        'nhomVatTu'=>'09. Thuốc vật tư y tế',
+        'tenThuongMai'=>'Hoàng kỳ chích mật',
+        'hoatChat'=>'Hoàng kỳ chích mật',
+        'donVi'=>'gram',
+        'duongDung'=>'',
+        'soLuong'=>12345,
+        'donGiaNhap'=>'',
+        'donGiaBan'=>220,
+        'ngaySanXuat'=>'2023-11-01 00:00:00',
+        'ngayHetHan'=>'2023-11-01 00:00:00',
+        'nhaSanXuat'=>'CTY dược ABC'
+    ),
+    array(
+        'maVatTu'=>'YHCT23007',
+        'nhomVatTu'=>'09. Thuốc vật tư y tế',
+        'tenThuongMai'=>'Xích thược',
+        'hoatChat'=>'Xích thược',
+        'donVi'=>'gram',
+        'duongDung'=>'',
+        'soLuong'=>12345,
+        'donGiaNhap'=>'',
+        'donGiaBan'=>220,
+        'ngaySanXuat'=>'2023-11-01 00:00:00',
+        'ngayHetHan'=>'2023-11-01 00:00:00',
+        'nhaSanXuat'=>'CTY dược ABC'
+    )
+));
